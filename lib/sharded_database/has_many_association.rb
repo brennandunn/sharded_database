7e172d6601da46ca8bbbc05330c1d49f6946a957
Brennan Dunn <me@brennandunn.com>
2009-02-12 15:03:22 -0500

    Shouldn't have had it apply to HasManyThrough associations yet. has_many :through capacity to come later.

--- lib/sharded_database/has_many_association.rb
+++ lib/sharded_database/has_many_association.rb
@@ -3,12 +3,11 @@ module ShardedDatabase
     
     def self.included(klass)
       klass.class_eval do
-        alias_method_chain :initialize, :connection
-        #alias_method_chain :find, :connection
+        alias_method_chain :initialize, :delegated_connection
       end
     end
     
-    def initialize_with_connection(owner, reflection)
+    def initialize_with_delegated_connection(owner, reflection)
       if @connection_class = reflection.options[:connection_class]
         @original_class = reflection.klass
         reflection.metaclass.class_eval %{
@@ -17,21 +16,10 @@ module ShardedDatabase
           end
         }
       end
-      initialize_without_connection(owner, reflection)
-    end
-    
-    def find_with_connection(*args)
-      if connection_class = @reflection.options[:connection_class]
-        ShardedDatabase::ModelWithConnection.borrow_connection(@reflection.klass, connection_class) do
-          find_without_connection(*args)
-        end
-      else
-        find_without_connection(*args)
-      end
+      initialize_without_delegated_connection(owner, reflection)
     end
     
   end
 end
 
 ActiveRecord::Associations::HasManyAssociation.send :include, ShardedDatabase::HasManyAssociation
-ActiveRecord::Associations::HasManyThroughAssociation.send :include, ShardedDatabase::HasManyAssociation
